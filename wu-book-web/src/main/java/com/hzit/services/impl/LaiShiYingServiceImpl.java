a715df85b263d30e2d44eb422e6943cffe6293b6
jasonlai <447312556@qq.com>
2016-09-30 13:41:11 +0800

    lai

--- wu-book-web/src/main/java/com/hzit/services/impl/LaiShiYingServiceImpl.java
+++ wu-book-web/src/main/java/com/hzit/services/impl/LaiShiYingServiceImpl.java
@@ -8,4 +8,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LaiShiYingServiceImpl implements LaiShiYingService {
+    @Override
+    public Object into() {
+        return null;
+    }
 }
