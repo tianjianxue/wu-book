afc132f259273afb3744fb671a4d72c9f578104d
baiyi2016 <739512491@qq.com>
2016-09-30 12:03:00 +0800

    baiyi

---
+++ wu-book-web/src/main/java/com/hzit/services/impl/BaiyiServicesImpl.java
@@ -0,0 +1,14 @@
+package com.hzit.services.impl;
+
+import com.hzit.services.BaiYiService;
+
+/**
+ * Created by Administrator on 2016/9/30.
+ */
+public class BaiyiServicesImpl implements BaiYiService{
+
+    @Override
+    public void baiyi() {
+
+    }
+}
