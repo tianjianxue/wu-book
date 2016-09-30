be9156bc5bd45ec015e46c8e686611e06f5f2179
qingguanfeng <619595145@qq.com>
2016-09-30 14:11:24 +0800

    覃光峰修改过 的

--- wu-book-web/src/main/java/com/hzit/services/QinGuangFengService.java
+++ wu-book-web/src/main/java/com/hzit/services/QinGuangFengService.java
@@ -1,8 +1,11 @@
 package com.hzit.services;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Administrator on 2016/9/30.
  */
+
 public interface QinGuangFengService {
     public void look();
 }
