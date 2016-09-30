7267b4570d177d31b4d7c3599585c200f2da7c39
wangguangjun <550234256@qq.com>
2016-09-30 12:19:49 +0800

    王光俊页面

--- wu-book-web/src/main/java/com/hzit/services/impl/WgjSerivceImpl.java
+++ wu-book-web/src/main/java/com/hzit/services/impl/WgjSerivceImpl.java
@@ -1,10 +1,12 @@
 package com.hzit.services.impl;
 
 import com.hzit.services.WgjSerivce;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2016/9/30.
  */
+@Service
 public class WgjSerivceImpl implements WgjSerivce {
 
     @Override
