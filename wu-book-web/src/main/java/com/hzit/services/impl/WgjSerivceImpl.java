6b8539486abf1f75508110987cf732180cbe9d5b
wangguangjun <550234256@qq.com>
2016-09-30 12:16:57 +0800

    王光俊页面

--- wu-book-web/src/main/java/com/hzit/services/impl/WgjSerivceImpl.java
+++ wu-book-web/src/main/java/com/hzit/services/impl/WgjSerivceImpl.java
@@ -2,20 +2,13 @@ package com.hzit.services.impl;
 
 import com.hzit.services.WgjSerivce;
 
-import org.springframework.stereotype.Service;
-
-
 /**
  * Created by Administrator on 2016/9/30.
  */
+public class WgjSerivceImpl implements WgjSerivce {
 
-@Service
-class WgjServiceImpl implements WgjSerivce {
-
-
-    public void  find() {
+    @Override
+    public void find() {
         System.out.println("haha");
-
-
     }
 }
