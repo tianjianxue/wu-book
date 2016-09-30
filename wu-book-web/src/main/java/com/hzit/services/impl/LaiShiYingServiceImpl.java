e28a4141373433e46f98d463ac2619a80c8b8098
jasonlai <447312556@qq.com>
2016-09-30 13:35:59 +0800

    lai

--- wu-book-web/src/main/java/com/hzit/services/impl/LaiShiYingServiceImpl.java
+++ wu-book-web/src/main/java/com/hzit/services/impl/LaiShiYingServiceImpl.java
@@ -1,10 +1,11 @@
 package com.hzit.services.impl;
 
+import com.hzit.services.LaiShiYingService;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2016/9/30.
  */
 @Service
-public class LaiShiYingServiceImpl {
+public class LaiShiYingServiceImpl implements LaiShiYingService {
 }
