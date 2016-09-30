061b0f76d9f4447472536248cf0c618976d3a542
wuwenjie <wuwenjie@hzitxx.com>
2016-09-30 15:05:38 +0800

    本地保存先

--- wu-book-web/src/main/java/com/wuwenjie/web/controller/HuangKangLuanController.java
+++ wu-book-web/src/main/java/com/wuwenjie/web/controller/HuangKangLuanController.java
@@ -4,8 +4,10 @@ import com.hzit.services.impl.HuangKangLuanServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.Date;
 
 /**
@@ -17,7 +19,11 @@ public class HuangKangLuanController {
     @Autowired
     private HuangKangLuanServiceImpl huangKangLuanService;
     @RequestMapping("/login")
-    public String login(){
+    public String login(@RequestParam("username") String usename,@RequestParam("password") String password,HttpSession session){
+        if(usename.equals(password)){
+            //把信息存储到session
+            session.setAttribute("user",usename);
+        }
         return "huangkangluan";
     }
     @RequestMapping("/time")
