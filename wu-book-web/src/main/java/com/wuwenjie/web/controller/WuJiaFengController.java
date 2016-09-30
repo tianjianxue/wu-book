0fc16f96a9ef09859f0172e68862b8977ad1fbcc
wujiafeng <1364291059@qq.com>
2016-09-30 15:56:01 +0800

    拦截器

--- wu-book-web/src/main/java/com/wuwenjie/web/controller/WuJiaFengController.java
+++ wu-book-web/src/main/java/com/wuwenjie/web/controller/WuJiaFengController.java
@@ -1,24 +1,34 @@
 package com.wuwenjie.web.controller;
 
 import com.hzit.services.WuJiaFengServices;
+import com.myinterceptor.WujiaFengInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.Date;
 
 /**
  * Created by Administrator on 2016/9/30.
  */
 @Controller
 @RequestMapping("/wujiafeng")
-public class WuJiaFengController {
+public class WuJiaFengController extends WebMvcConfigurerAdapter{
     @Autowired
     private WuJiaFengServices wuJiaFengServices;
     public WuJiaFengController(){
         System.out.println("吴佳峰的控制器对象被创建");
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new WujiaFengInterceptor()).addPathPatterns("/wujiafeng/**");
+        super.addInterceptors(registry);
+    }
+
     @RequestMapping("/currenttime")
     @ResponseBody
     public Object time(){
@@ -27,9 +37,9 @@ public class WuJiaFengController {
         return wuJiaFengServices.time();
     }
     @RequestMapping("/login")
-    public String login(){
+    public String login(ModelMap modelMap){
         System.out.println("处理用户登录的Controller");
-
+        modelMap.put("user","张无忌");
         return "wujiafeng";
     }
 }
