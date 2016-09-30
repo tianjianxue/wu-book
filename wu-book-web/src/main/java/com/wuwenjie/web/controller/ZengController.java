ba7c1142885492d1d719568fb168efc2b3a4223c
zengchao <uljaxsec1991@163.com>
2016-09-30 15:35:40 +0800

    提交到本地

--- wu-book-web/src/main/java/com/wuwenjie/web/controller/ZengController.java
+++ wu-book-web/src/main/java/com/wuwenjie/web/controller/ZengController.java
@@ -2,16 +2,25 @@ package com.wuwenjie.web.controller;
 
 import com.hzit.services.ZengService;
 import com.hzit.services.impl.ZengServiceImpl;
+import com.myinterceptor.ZengInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Administrator on 2016/9/30.
  */
 @Controller
 @RequestMapping("/zeng")
-public class ZengController {
+public class ZengController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ZengInterceptor()).addPathPatterns("/zeng/**");
+        super.addInterceptors(registry);
+    }
 
     @Autowired
     private ZengService zengService;
@@ -36,4 +45,5 @@ public class ZengController {
         System.out.println("我要删除了");
         return "zengchao";
     }
+
 }
