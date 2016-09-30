f42f0f43bea56a20266c22e8acebb96caa2fcab9
wangguangjun <550234256@qq.com>
2016-09-30 12:14:24 +0800

    王光俊页面

--- wu-book-web/src/main/java/com/hzit/services/WgjSerivce.java
+++ wu-book-web/src/main/java/com/hzit/services/WgjSerivce.java
@@ -1,8 +1,8 @@
-package com.hzit.services.impl;
+package com.hzit.services;
 
 /**
  * Created by Administrator on 2016/9/30.
  */
-public interface WangGuangJunServiceImpl {
+public interface WgjSerivce {
     public void find();
 }
---
+++ wu-book-web/src/main/java/com/hzit/services/impl/WgjServiceImpl.java
@@ -0,0 +1,21 @@
+package com.hzit.services.impl;
+
+import com.hzit.services.WgjSerivce;
+
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Created by Administrator on 2016/9/30.
+ */
+
+@Service
+class WgjServiceImpl implements WgjSerivce {
+
+
+    public void  find() {
+        System.out.println("haha");
+
+
+    }
+}
